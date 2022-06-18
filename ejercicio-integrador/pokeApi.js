590f3cceae2b26477ba81de731ea254b53470ebe
u921220 <matias.navarreteutn@gmail.com>
2022-06-18 09:33:38 -0300

    feat: ejercicio integrador

---
+++ ejercicio-integrador/pokeApi.js
@@ -0,0 +1,82 @@
+/**
+ * !=======================================
+ * !Estamos llegando a la finalización del curso de Javascript Vanilla
+ * !y vamos a realizar un repaso de los temas vistos mediante un ejercicio
+ * !integrador conectadonos con Api...
+ *
+ * !Para eso deberas realizar las siguientes consignas siguiendo las buenas practicas vistas en clase
+ * !y utilizando todo el conocimiento aprendido en clases anteriores...
+ *
+ * * ...
+ *
+ * !Comencemos.
+ * !===========================================
+ */
+
+/**
+ * *=====================================
+ * *            PRIMERA  PARTE
+ * *-------------------------------------
+ */
+
+/**
+ * Vamos a trabajar con PokeApi. La Api de Pokemon.
+ * ? 1) Almacenar la base url en una constante.
+ */
+
+// todo: todo: Code here
+
+/**
+ * ? 2) Crear la configuración de un fetch para ejecutar en una consola del navegador.
+ */
+
+// todo: Code here
+
+/**
+ * ? 3) Realizar una busqueda de los primeros 20 pokemones
+ */
+
+// todo: Code here
+
+
+/**
+ * ? 4) Buscar al pokemon 'Charmander', armar un objeto literal con las siguientes propiedades de este pokemon:
+ *      id, name, tipo y almenos 2 movimientos
+*       Luego mostrar por consola
+ */
+
+// todo: Code here
+
+/**
+ * ? 5) Obtener la cadena de evoluciones de Charmander
+ */
+
+// todo: Code here
+
+/**
+ * *=====================================
+ * *            SEGUNDA  PARTE
+ * *-------------------------------------
+ */
+
+
+/**
+ * ? 6) Indicar cual es el id del tipo psiquico. Armar un objeto con las características que consideres importantes del tipo electrico
+ */
+
+// todo: Code here
+
+/**
+ * ? 7) Indicar cuantos pokemons electricos hay, y crear objetos literales con alguna descripción de 5 de estos pokemons electricos
+ */
+
+// todo: Code here
+
+/**
+ * ? 8) Que hay de particular en la especie del pokemon Lugia
+ */
+
+// todo: Code here
+
+
+
